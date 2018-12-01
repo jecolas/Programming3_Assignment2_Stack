e52801dc47c614911849cc4eac6d2415686c4a12
jecolas <34898937+jecolas@users.noreply.github.com>
2018-12-01 10:14:33 -0500

    initial commit

---
+++ MHagiyama_COMP212_Sec001_StackInheritanceTest/Moemi-Hagiyama_StackInheritanceTest/StackInheritanceTest.cs
@@ -0,0 +1,51 @@
+﻿// Fig. 19.14: StackInheritanceTest.cs
+// Testing class StackInheritance.
+using System;
+using StackInheritanceLibrary;
+using LinkedListLibrary;
+
+// demonstrate functionality of class StackInheritance
+class StackInheritanceTest
+{
+   static void Main()
+   {
+      StackInheritance<double> stack = new StackInheritance<double>();
+
+      // use method Push to add items to stack
+      Console.WriteLine("==============push==============");
+      stack.Push(10.5);
+      stack.Display();
+      stack.Push(13.25);
+      stack.Display();
+      stack.Push(50.87);
+      stack.Display();
+      stack.Push(89.21);
+      stack.Display();
+
+        Console.WriteLine("==============execute peek method==============");
+        stack.Peek();
+
+        Console.WriteLine("==============pop==============");
+        stack.Pop();
+        stack.Display();
+        stack.Pop();
+        stack.Display();
+
+      // remove items from stack
+      try
+      {
+         while (true)
+         {
+            object removedObject = stack.Pop();
+            Console.WriteLine($"{removedObject} popped");
+            stack.Display();
+         }
+      }
+      catch (EmptyListException emptyListException)
+      {
+         // if exception occurs, write stack trace
+         Console.Error.WriteLine(emptyListException.StackTrace);
+      }
+   }
+}
+
---
+++ Moemi-Hagiyama_COMP212_Sec001_StackInheritanceLibrary/Hagiyama_StackInheritanceLibrary/StackInheritance.cs
@@ -0,0 +1,33 @@
+﻿// Fig. 19.13: StackInheritanceLibrary.cs
+// Implementing a stack by inheriting from class List.
+using LinkedListLibrary;
+
+namespace StackInheritanceLibrary
+{
+   // class StackInheritance inherits class List's capabilities
+   public class StackInheritance<T> : List<T>
+   {
+      // pass name "stack" to List constructor
+      public StackInheritance() : base("stack") { }
+
+      // place dataValue at top of stack by inserting 
+      // dataValue at front of linked list
+      public void Push(T dataValue)
+      {
+            InsertAtFront(dataValue);
+      }
+
+      // remove item from top of stack by removing
+      // item at front of linked list
+      public T Pop()
+      {
+         return RemoveFromFront();
+      }
+
+        public void Peek()
+        {
+            System.Console.WriteLine(getFirst());
+        } 
+   }
+}
+
